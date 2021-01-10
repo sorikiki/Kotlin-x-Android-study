de6b610255b54327d2067a74fcb88379d991ae05
sorikiki <sorikiki98@naver.com>
2021-01-10 18:39:57 +0900

    intro to Kotlin

--- 1. Android Basics in Kotlin/1. Introduction to Kotlin.kt
+++ 1. Android Basics in Kotlin/1. Introduction to Kotlin.kt
@@ -1,4 +1,5 @@
-// ✅ Basic elements: In Kotlin, we need a 'main' function. 
+// ✅ Basic elements: main() & println()
+// main() is an entry point of a Kotlin application.
 fun main() {
     println("Hello, world!")
     println("Dasol Kim")
@@ -7,4 +8,65 @@ fun main() {
 
 // ✔ println() vs print()
 // => print(): text without line break.
-// => we use 1) '\n' or 2) println("") instead.
+// => we use 1) '\n' or 2) println() instead.
+
+// ✅ Basic elements: variable
+// ❗ Remember that read-only local variables are defined using the keyword 'val'. They can be assigned a value only once.
+// ❗ 'var' is able to make us modify previous value.
+fun main() {
+    val age = 24
+    val name = "Dasol"
+    print("Happy Birthday, ${name}\n")
+	// let's print a cake!
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+	// This prints an empty line
+    println("")
+    println("You are already ${age} old, ${name}!")
+    println("${age} is the very best age to celebrate")
+}
+
+// ✅ Application: functions
+// 1. low-level function into main()
+fun main() {
+	printBorder()
+    println("Happy Birthday, Dasol!")
+    printBorder()
+}
+
+fun printBorder() {
+        println("=======================")
+}
+
+// 2. repeat() = loop
+fun main() {
+	printBorder()
+    println("Happy Birthday, Dasol!")
+    printBorder()
+}
+
+fun printBorder() {
+        repeat(23) {
+            print("=")
+        } // This iterates same code 'print("=")'!
+        println()
+        }
+
+// 3. use arguments
+fun main() {
+    val border = "%"
+	printBorder(border)
+    println("Happy Birthday, Dasol!")
+    printBorder(border)
+}
+
+fun printBorder(arg: String) {
+        repeat(23) {
+            print(arg)
+        } // This iterates same code 'print("=")'!
+        println()
+        }
