6b37884f151cc97700f8236fdeb4aefffbb82845
sorikiki <sorikiki98@naver.com>
2021-01-10 23:12:26 +0900

    view and viewgroups

---
+++ 1. Android Basics in Kotlin/2. Basic layout.kt
@@ -0,0 +1,17 @@
+// ✅ View and Viewgroups 
+// The UI  of an app is what you see on the screen: text, images, buttons, and many other types of elements.
+// Each of these elements is what's called a 'View'
+// For example, you will work with a kind of View that is for displaying text and is called a 'TextView'
+// To organize Views, you put them in a container.
+// A 'ViewGroup' is a container that View objects can sit in, and is responsible for arranging the Views inside it.
+// The arrangement, or layout, can change depending on the size and aspect ratio of the screen of the Android device that the app is running on, and the layout can adapt to whether the device is in portrait or landscape mode.
+// One kind of ViewGroup is a 'ConstraintLayout', which helps you arrange the Views inside it in a flexible way.
+// Views need to be constrained horizontally and vertically within a ConstraintLayout.
+// ✨ Layout Editor: it helps you create the UI for your Android app
+// => It supports code, split, design modes. 
+
+// cf. the purpose of the activity_main.xml file in the project you created: 
+// => It describes the layout of view groups and views for a screen.
+
+// Note: A constraint in the context of the Layout Editor represents a connection or alignment of a view to another view, the parent layout, or an invisible guideline. 
+// For example, a view can be constrained to be a certain distance from the edge of its container, or always be to the right of another view, or always the top view inside a container.
