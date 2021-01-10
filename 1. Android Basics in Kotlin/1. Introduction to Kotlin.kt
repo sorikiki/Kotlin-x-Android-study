132600704fdf59f10e3da61d5dfb21316680036d
sorikiki <sorikiki98@naver.com>
2021-01-10 16:57:54 +0900

    Kotlin Basic elements

---
+++ 1. Android Basics in Kotlin/1. Introduction to Kotlin.kt
@@ -0,0 +1,10 @@
+// ✅ Basic elements: In Kotlin, we need a 'main' function. 
+fun main() {
+    println("Hello, world!")
+    println("Dasol Kim")
+    println("You are 24!")
+} 
+
+// ✔ println() vs print()
+// => print(): text without line break.
+// => we use 1) '\n' or 2) println("") instead.
