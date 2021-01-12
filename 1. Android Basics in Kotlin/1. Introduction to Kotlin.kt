c575a83eeddb4fc49d5e52613def9b16a5b1eb30
sorikiki <sorikiki98@naver.com>
2021-01-12 18:45:18 +0900

    conditionals

--- 1. Android Basics in Kotlin/1. Introduction to Kotlin.kt
+++ 1. Android Basics in Kotlin/1. Introduction to Kotlin.kt
@@ -155,3 +155,66 @@ class Dice(val numSides: Int) {
 }
 // ✔ Notice that we add () after class name, and declare 'val ... : type'
 
+// ✅ Conditionals: if, else if, else, when ✨
+fun main() {
+    val num = 4
+    if (num > 4) {
+        println("The variable is greater than 4")
+    } else if (num == 4) {
+        println("The variable is equal to 4")
+    } else {
+        println("The variable is less than 4")
+    }
+}
+
+// 😡 Too long. 
+fun main() {
+    val myFirstDice = Dice(6)
+    val diceRoll = myFirstDice.roll()
+    val luckyNumber= 4
+    if (diceRoll == luckyNumber) {
+        println("You win!")
+    } else if (rollResult == 1) {
+        println("So sorry! You rolled a 1. Try again!")
+    } else if (rollResult == 2) {
+        println("Sadly, you rolled a 2. Try again!")
+    } else if (rollResult == 3) {
+        println("Unfortunately, you rolled a 3. Try again!")
+    } else if (rollResult == 4) {
+        println("No luck! You rolled a 4. Try again!")
+    } else if (rollResult == 5) {
+        println("Don't cry! You rolled a 5. Try again!")
+    } else {
+        println("Apologies! you rolled a 6. Try again!")
+    }
+}
+
+class Dice (val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
+
+// 😊 There are several situations when we use a lot of 'else if' sentences. Then use 'when'. 💖
+fun main() {
+    val myFirstDice = Dice(6)
+    val rollResult = myFirstDice.roll()
+    val luckyNumber = 4
+
+    when (rollResult) {
+        luckyNumber -> println("You won!")
+        1 -> println("So sorry! You rolled a 1. Try again!")
+        2 -> println("Sadly, you rolled a 2. Try again!")
+        3 -> println("Unfortunately, you rolled a 3. Try again!")
+        4 -> println("No luck! You rolled a 4. Try again!")
+        5 -> println("Don't cry! You rolled a 5. Try again!")
+        6 -> println("Apologies! you rolled a 6. Try again!")
+    }
+}
+// Read this as, "If rollResult is luckyNumber, then print the "You win!" message."
+// ❓ when the value of the when expression is assigned to a variable, the 'when' must be exhaustive. 
+// ❗ Let's add necessary ‘else' branch.
+
+
+
