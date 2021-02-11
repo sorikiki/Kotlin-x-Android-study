5c5ab5a5154376269d18ed17f57dec6e2549b31e
sorikiki <sorikiki98@naver.com>
2021-02-12 01:27:34 +0900

    Before learning to SQLite3

---
+++ 2. Extra study/2. SQL primer.kt
@@ -0,0 +1,15 @@
+// ✅ SQLite3
+// It is very common for Android apps to store and load user data. 
+// Because all data stored in memory during the app's execution disappears when the app's execution ends, the data that must be maintained regardless of whether the app runs must be stored in a permanently archiveable repository to prevent data from being deleted.
+// => SQLite is a database management system (DBMS) that is suitable for processing relatively small amounts of data in a single application rather than for server-specific bulk data, which is a database management system that is suitable for use when database management is needed in small programs like Android apps.
+
+// In Android, data is represented in data classes. This data is accessed and possibly modified using function calls. 
+// However, in the database world, you need "entities" and "queries" to access and modify data.
+
+// The basic thing to know about using SQLite on Android is, of course, the structure of the class that deals with SQLite and the usage of API functions. But before that, you need to understand the basic concepts and components of the database and the Structured Query Language (SQL) used to manage the data in the database.
+
+// What you'll learn
+// ◽ Databases in general
+// ◽ SQL databases in particular
+// ◽ The SQL language used to interact with databases
+gi
